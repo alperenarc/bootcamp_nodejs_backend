f2f4df5dbdac39d44c1e750709f3721932d668bb
KRCTEST\alperen.arici <alparicieren@gmail.com>
2020-11-18 18:01:55 +0300

    Controllers and routes folder added

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+// Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+const logger = (res, req, next) => {
+    req.hello = 'hello World';
+    console.log('middleware run');
+    next();
+}
+app.use(logger);
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
