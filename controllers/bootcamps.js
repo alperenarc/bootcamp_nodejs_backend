9a3e4a0778b9981fecef6f458c3e59d2fc891be3
KRCTEST\alperen.arici <alparicieren@gmail.com>
2020-11-19 11:55:51 +0300

    Get bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,32 @@ const Bootcamp = require('../models/Bootcamp');
 // @description     Get All bootcamps
 // @route           GET /api/v1/bootcamps
 // @access          Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps });
+
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+
 }
 
 // @description     Get single bootcamp
 // @route           GET /api/v1/bootcamps:/id
 // @access          Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            res.status(400).json({ success: false, message: 'No element for this id' });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 }
 
 // @description     Create a new bootcamp
