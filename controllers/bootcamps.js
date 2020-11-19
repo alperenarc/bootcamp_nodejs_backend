05991769c6402c12422daa6020106be278f538fc
KRCTEST\alperen.arici <alparicieren@gmail.com>
2020-11-19 11:48:20 +0300

    Create bootcamp method added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @description     Get All bootcamps
 // @route           GET /api/v1/bootcamps
 // @access          Public
@@ -15,8 +17,21 @@ exports.getBootcamp = (req, res, next) => {
 // @description     Create a new bootcamp
 // @route           POST /api/v1/bootcamps
 // @access          Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+
+        });
+    }
+
 }
 // @description     Update bootcamp
 // @route           UPDATE /api/v1/bootcamps:/id
