74879ed2946a6f19e96d03f59af710427ec163db
KRCTEST\alperen.arici <alparicieren@gmail.com>
2020-11-26 18:22:42 +0300

    Role authorization

--- routes/courses.js
+++ routes/courses.js
@@ -5,16 +5,20 @@ const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResult');
 
 const router = express.Router({ mergeParams: true });
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 router
     .route('/')
     .get(advancedResults(Course, {
         path: 'bootcamp',
         select: 'name description'
     }), getCourses)
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 
-router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router
+    .route('/:id')
+    .get(getCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 
 module.exports = router;
